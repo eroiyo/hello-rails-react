2be51042379fff432a400a085b1dfe17558fe351
eroiyo <eroiyo@outlook.es>
2021-12-07 14:42:48 -0400

    start again

--- app/controllers/v1/things_controller.rb
+++
@@ -1,9 +0,0 @@
-class V1::ThingsController < ApplicationController
-    def index
-        render json: { :things => [
-            {:name => 'something',
-            :guid => '0415674d-a0e8-4748-af68a5f53a05c453'
-        }
-        ] }.to_json
-    end
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  namespace :v1, defaults: { format: 'json' } do
-    get 'things', to: 'things#index'
-  end
-end
