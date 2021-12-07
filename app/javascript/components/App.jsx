9dd80b79db7a1f1b8d90c5901e6c6538e354042c
eroiyo <eroiyo@outlook.es>
2021-12-07 09:47:43 -0400

    install react

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = (props) => {
+    return <div> HELLO from react</div>
+}
+
+export default App
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
